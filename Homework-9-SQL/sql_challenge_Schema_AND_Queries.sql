ad18111403f2676a5abf34d49fdd8b6495670c72
Luan Hajnaj <52753636+lhajnaj@users.noreply.github.com>
2019-10-01 16:45:06 -0400

    Update sql_challenge_Schema_AND_Queries.sql

--- Homework-9-SQL/sql_challenge_Schema_AND_Queries.sql
+++ Homework-9-SQL/sql_challenge_Schema_AND_Queries.sql
@@ -1,19 +1,18 @@
--- Drop Tables if Existing
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
+--Create Tables and Import CSV Data:
 
+DROP TABLE IF EXISTS departments;
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+    CONSTRAINT "primary_key_departments" PRIMARY KEY ("dept_no" )
 );
+
+SELECT * FROM departments;
+
+--
+
+DROP TABLE IF EXISTS dept_emp;
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
@@ -22,12 +21,24 @@ CREATE TABLE "dept_emp" (
     "to_date" DATE   NOT NULL
 );
 
+SELECT * FROM dept_emp;
+
+--
+
+DROP TABLE IF EXISTS dept_manager;
+
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR   NOT NULL,
     "emp_no" INT   NOT NULL,
     "from_date" DATE   NOT NULL,
     "to_date" DATE   NOT NULL
 );
+
+SELECT * FROM dept_manager;
+
+--
+
+DROP TABLE IF EXISTS employees;
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
@@ -36,10 +47,14 @@ CREATE TABLE "employees" (
     "last_name" VARCHAR   NOT NULL,
     "gender" VARCHAR   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+    CONSTRAINT "primary_key_employees" PRIMARY KEY ("emp_no")
 );
+
+SELECT * FROM employees;
+
+--
+
+DROP TABLE IF EXISTS salaries;
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
@@ -48,6 +63,12 @@ CREATE TABLE "salaries" (
     "to_date" DATE   NOT NULL
 );
 
+SELECT * FROM salaries;
+
+--
+
+DROP TABLE IF EXISTS titles;
+
 CREATE TABLE "titles" (
     "emp_no" INT   NOT NULL,
     "title" VARCHAR   NOT NULL,
@@ -55,23 +76,25 @@ CREATE TABLE "titles" (
     "to_date" DATE   NOT NULL
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+SELECT * FROM titles;
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+-- Contraints and References added:
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "foreign_key_dept_emp_emp_no" FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "foreign_key_dept_emp_dept_no" FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "foreign_key_dept_manager_dept_no" FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "foreign_key_dept_manager_emp_no" FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "foreign_key_salaries_emp_no" FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "foreign_key_titles_emp_no" FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no");
+
+
 
 --Confirm proper importing of CSV files:
 SELECT * FROM departments;
@@ -81,63 +104,36 @@ SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
 
+
+
 -- 1) List the following details of each employee: employee number, last name, first name, gender, and salary.
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
-FROM employees
-JOIN salaries
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary FROM employees JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
 -- 2) List employees who were hired in 1986.
-SELECT first_name, last_name, hire_date 
-FROM employees
+SELECT first_name, last_name, hire_date FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
 
 -- 3) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name, dept_manager.from_date, dept_manager.to_date
-FROM departments
-JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no
-JOIN employees
-ON dept_manager.emp_no = employees.emp_no;
+FROM departments JOIN dept_manager ON departments.dept_no = dept_manager.dept_no JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
 -- 4) List the department of each employee with the following information: employee number, last name, first name, and department name.
-SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM dept_emp
-JOIN employees
-ON dept_emp.emp_no = employees.emp_no
-JOIN departments
-ON dept_emp.dept_no = departments.dept_no;
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name FROM dept_emp JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no;
 
 -- 5) List all employees whose first name is "Hercules" and last names begin with "B."
-SELECT first_name, last_name
-FROM employees
-WHERE first_name = 'Hercules'
-AND last_name LIKE 'B%';
+SELECT first_name, last_name FROM employees WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- 6) List all employees in the Sales department, including their employee number, last name, first name, and department name.
-SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM dept_emp
-JOIN employees
-ON dept_emp.emp_no = employees.emp_no
-JOIN departments
-ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_name = 'Sales';
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name FROM dept_emp JOIN employees
+ON dept_emp.emp_no = employees.emp_no JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE departments.dept_name = 'Sales';
 
 -- 7) List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM dept_emp
-JOIN employees
-ON dept_emp.emp_no = employees.emp_no
-JOIN departments
-ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_name = 'Sales' 
-OR departments.dept_name = 'Development';
+FROM dept_emp JOIN employees ON dept_emp.emp_no = employees.emp_no JOIN departments
+ON dept_emp.dept_no = departments.dept_no WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
 
 -- 8) In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name,
-COUNT(last_name) AS "frequency"
-FROM employees
-GROUP BY last_name
-ORDER BY
-COUNT(last_name) DESC;
+SELECT last_name, COUNT(last_name) AS "frequency" FROM employees GROUP BY last_name ORDER BY COUNT(last_name) DESC;
 
